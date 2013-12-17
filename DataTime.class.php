b120056bafec81257c6605ab951e089e44dff358
bnu <bnufactory@gmail.com>
2013-12-17 21:14:25 +0900

    NOISSUE 코드 정리.

--- DataTime.class.php
+++ DataTime.class.php
@@ -48,4 +48,3 @@ class DataTime{
 		}
 	}
 }
-?>
--- kin.class.php
+++ kin.class.php
@@ -1,37 +1,46 @@
 <?php
-    /**
-     * @class  kin
-     * @author zero (skklove@gmail.com)
-     * @brief  kin high class
-     **/
+/**
+ * @class  kin
+ * @author zero (skklove@gmail.com)
+ * @brief  kin high class
+ **/
 
-    class kin extends ModuleObject {
+class kin extends ModuleObject
+{
+	var $search_option = array('title','content','title_content','comment','user_name','nick_name','user_id','tag');
+	var $list_count = 20;
+	var $page_count = 10;
 
-        var $search_option = array('title','content','title_content','comment','user_name','nick_name','user_id','tag'); 
-		var $list_count = 20;
-		var $page_count = 10; 
+	function moduleInstall()
+	{
+		$oModuleController = &getController('module');
+		$oModuleController->insertTrigger('menu.getModuleListInSitemap', 'kin', 'model', 'triggerModuleListInSitemap', 'after');
+		return new Object();
+	}
 
-        function moduleInstall() {
-			$oModuleController = &getController('module');
+	function checkUpdate()
+	{
+		$oModuleModel = &getModel('module');
+		if(!$oModuleModel->getTrigger('menu.getModuleListInSitemap', 'kin', 'model', 'triggerModuleListInSitemap', 'after'))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	function moduleUpdate()
+	{
+		$oModuleModel = &getModel('module');
+		$oModuleController = &getController('module');
+		if(!$oModuleModel->getTrigger('menu.getModuleListInSitemap', 'kin', 'model', 'triggerModuleListInSitemap', 'after'))
+		{
 			$oModuleController->insertTrigger('menu.getModuleListInSitemap', 'kin', 'model', 'triggerModuleListInSitemap', 'after');
-            return new Object();
-        }
+		}
+		return new Object(0, 'success_updated');
+	}
 
-        function checkUpdate() {
-			$oModuleModel = &getModel('module');
-			if(!$oModuleModel->getTrigger('menu.getModuleListInSitemap', 'kin', 'model', 'triggerModuleListInSitemap', 'after')) return true;
-            return false;
-        }
-
-        function moduleUpdate() {
-			$oModuleModel = &getModel('module');
-			$oModuleController = &getController('module');
-			if(!$oModuleModel->getTrigger('menu.getModuleListInSitemap', 'kin', 'model', 'triggerModuleListInSitemap', 'after'))
-				$oModuleController->insertTrigger('menu.getModuleListInSitemap', 'kin', 'model', 'triggerModuleListInSitemap', 'after');
-            return new Object(0, 'success_updated');
-        }
-
-        function recompileCache() {
-        }
-    }
-?>
+	function recompileCache()
+	{
+	}
+}
